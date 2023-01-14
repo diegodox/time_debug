a7f4254e2175f1e702429d667fc2e34657d1aa08
diegodox <android.mxdiego9@gmail.com>
2023-01-15 00:01:15 +0900

    add with_time

--- src/lib.rs
+++ src/lib.rs
@@ -34,6 +34,33 @@ macro_rules! time {
     }};
 }
 
+/// Mesure time to execute given expression and returns the value of a given expression
+/// paired with expression's return value for quick and dirty time mesuring.
+///
+/// # Example:
+///
+/// ```
+/// # use time_debug::with_time;
+/// # use std::thread::sleep;
+/// # use std::time::Duration;
+/// let (time, _ret) = with_time!(sleep(Duration::new(2, 0)));
+/// ```
+///
+/// ```
+/// # use time_debug::with_time;
+/// let (time, ret) = with_time!(1 + 2);
+/// assert_eq!(ret, 3);
+/// ```
+#[macro_export]
+macro_rules! with_time {
+    ($fn:expr) => {{
+        let start_time = std::time::Instant::now();
+        let ret = $fn;
+        let time = start_time.elapsed();
+        (time, ret)
+    }};
+}
+
 pub use humantime::format_duration as _format_duration;
 
 #[test]
